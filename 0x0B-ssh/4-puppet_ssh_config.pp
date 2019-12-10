156f6109526b276369d36a3060efb5fe48044136
David Rosero <davidroserocalle@gmail.com>
2019-12-10 10:23:22 -0500

    a simple delacration of the puppet

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # Update the password authentication for not
-file_line {
+file_line { 'set on the passwd':
   path    => '/etc/ssh/ssh_config',
   line    => '    PasswordAuthentication no',
 }
 # Add the key
-file_line {
+file_line { 'Using the key':
   path    => '/etc/ssh/ssh_config',
   line    => '    IdentityFile ~/.ssh/holberton',
 }
