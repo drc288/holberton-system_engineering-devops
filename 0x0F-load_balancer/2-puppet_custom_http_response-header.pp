f39be365ae0649f0aa7fb8e5577f7a96e3efbb82
David Rosero C <davidroserocalle@gmail.com>
2020-01-06 15:55:28 +0000

    fix file and understand more poppet

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,31 +1,31 @@
-# This puppet file create and update the server and install nginx
-# setup a new header in nginx condiguration and set the value for
-# the $HOSTNAME
-# ##############################################################
-# #                         Puppet File                        #
-# ##############################################################
-#
-# Update the sistem
-exec { 'update-system':
-  command => '/usr/bin/apt-get update',
+etup a new header in nginx condiguration and set the value for
+# the $HOSTNAME
+# ##############################################################
+# #                         Puppet File                        #
+# ###############################################################
+# Update the sistem
+exec { 'update-system':
+  command => '/usr/bin/apt-get update',
 }
-# Install the deamon Nginx
-package { 'nginx':
-  ensure  => installed,
-  require => Exec['update-system'],
+# Install the deamon Nginx
+package { ‘nginx':
+  ensure  => installed,
+  require => Exec[‘update-system'],
 }
-# Running the service
-service { 'nginx':
-  ensure => running,
+# Running the service
+service { ‘nginx':
+  ensure    => running,
+  require   => Package[‘nginx'],
+  subscribe => Exec['add-header'],
 }
-# Create a file and insert basic html
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Holberton School',
-  require => Service['nginx'],
+# Create a file and insert basic html
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Holberton School’,
+  require => Service['nginx'],
 }
-# add header, using sed
-exec { 'add-header':
-  command => '/bin/sed -i \'s/_;/_;\n\tadd_header \'X-Served-By\' \"\$HOSTANME\"; /g\' /etc/nginx/sites-available/default',
-  require => Service['nginx'],
+# add header, using sed
+exec { ‘add-header':
+  command => '/bin/sed -i \'s/_;/_;\n\tadd_header X-Served-By \$HOSTNAME; /g\' /etc/nginx/sites-available/default', 
+  notify  => Service['nginx'],
 }
