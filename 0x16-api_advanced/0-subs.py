a013e4437612c762f6a29f99d993bfc2d28e505a
David Rosero C <davidroserocalle@gmail.com>
2020-02-06 10:53:14 -0500

    update

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This file send a request  to https://www.reddit.com and get
+the subredit
+"""
+import praw
+import requests
+
+def number_of_subscribers(subreddit):
+    """Get all subscribers for a subreddit
+        Input: Str(subreddit)
+        Out: Int(total of subscribers)
+    """
+    url = "https://www.reddit.com/r/"
+    header = {
+        'User-Agent': 'My user 1.0'
+    }
+    to_search = url + subreddit + "/about.json"
+
+    req = requests.get(to_search, headers=header)
+    if req.status_code != 200:
+        return 0
+    else:
+        dataJSON = req.json().get("data")
+        return dataJSON.get("subscribers")
